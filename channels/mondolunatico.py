3e6637c303353efc99ac99314aa18e9a3e7a8d0b
Dentaku65 <dentaku65@gmail.com>
2016-08-03 19:04:32 +0200

    small fix version

--- channels/mondolunatico.py
+++ channels/mondolunatico.py
@@ -206,8 +206,8 @@ def findvideos(item):
             ['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'],
             ['Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'],
             ['Accept-Language', 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3'],
-            #['Cookie', 'flag[' + id + ']=1; noadvtday=0; nopopatall=' + str(time.time())],
-            ['Cookie', 'flag[' + id + ']=1; PHPSESSID=([a-z0-9]+)]'],
+            ['Cookie', 'flag[' + id + ']=1; noadvtday=0; nopopatall=' + str(time.time())],
+            #['Cookie', 'flag[' + id + ']=1; PHPSESSID=([a-z0-9]+)]'],
             ['Referer', keeplinks + id],
             ['Accept-Encoding', 'gzip, deflate, br'],
             ['Connection', 'keep-alive']
