7e67dfc3f0e4958182bebba319f5238ab4490b4c
Dentaku65 <dentaku65@gmail.com>
2016-08-03 19:08:56 +0200

    v5.3.3

--- default.py
+++ default.py
@@ -9,8 +9,8 @@
 __plugin__  = "streamondemand"
 __author__  = "iSOD"
 __url__     = "http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/"
-__date__ = "10/07/2016"
-__version__ = "5.3.2"
+__date__ = "03/08/2016"
+__version__ = "5.3.3"
 
 import os
 import sys
