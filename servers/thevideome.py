922bb03a9c9aebd944e93e7a3c75406a7a1c3793
Dentaku65 <dentaku65@gmail.com>
2016-08-26 21:21:30 +0200

    Fix openload and thevideome

--- servers/thevideome.py
+++ servers/thevideome.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# ------------------------------------------------------------
+#------------------------------------------------------------
 # pelisalacarta - XBMC Plugin
 # Conector para thevideo.me
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
-# ------------------------------------------------------------
+# fixed by cmos
+#------------------------------------------------------------
 
 import re
 
@@ -11,19 +12,29 @@ from core import logger
 from core import scrapertools
 
 
-def get_video_url(page_url, premium=False, user="", password="", video_password=""):
-    logger.info("streamondemand.servers.thevideome url=" + page_url)
+def test_video_exists( page_url ):
+    logger.info("pelisalacarta.servers.thevideome test_video_exists(page_url='%s')" % page_url)
+    return True,""
 
-    data = scrapertools.cache_page(page_url)
-    media_urls = scrapertools.find_multiple_matches(data, r"'?label'?\s*:\s*'([^']+)p'\s*,\s*'?file'?\s*:\s*'([^']+)")
+def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
+    logger.info("pelisalacarta.servers.thevideome url="+page_url)
+    if not "embed" in page_url:
+        page_url = page_url.replace("http://thevideo.me/","http://thevideo.me/embed-") + ".html"
+    
+    data = scrapertools.cache_page( page_url )
+
+    tkn = scrapertools.find_single_match(data, "tkn='([^']+)'")
+    data_vt = scrapertools.downloadpage("http://thevideo.me/jwv/%s" % tkn)
+    data_vt = scrapertools.find_single_match(data_vt, 'jwConfig\|([^\|]+)\|')
+    
+    media_urls = scrapertools.find_multiple_matches(data,"label\s*\:\s*'([^']+)'\s*\,\s*file\s*\:\s*'([^']+)'")
     video_urls = []
 
     for label, media_url in media_urls:
-        video_urls.append(
-            [scrapertools.get_filename_from_url(media_url)[-4:] + " (" + label + ") [thevideo.me]", media_url])
+        media_url += "?direct=false&ua=1&vt=%s" % data_vt
+        video_urls.append( [ scrapertools.get_filename_from_url(media_url)[-4:]+" ("+label+") [thevideo.me]",media_url])
 
     return video_urls
-
 
 # Encuentra vídeos del servidor en el texto pasado
 def find_videos(data):
@@ -31,18 +42,32 @@ def find_videos(data):
     encontrados = set()
     devuelve = []
 
-    patronvideos = 'thevideo.me/(?:embed-)?([a-z0-9A-Z]+)'
-
-    logger.info("streamondemand.servers.thevideome find_videos #" + patronvideos + "#")
-    matches = re.compile(patronvideos, re.DOTALL).findall(data)
+    patronvideos  = 'thevideo.me/embed-([a-z0-9A-Z]+)'
+    logger.info("pelisalacarta.servers.thevideome find_videos #"+patronvideos+"#")
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match in matches:
         titulo = "[thevideo.me]"
-        url = "http://thevideo.me/embed-" + match + ".html"
+        url = "http://thevideo.me/embed-"+match+".html"
         if url not in encontrados:
-            logger.info("  url=" + url)
-            devuelve.append([titulo, url, 'thevideome'])
+            logger.info("  url="+url)
+            devuelve.append( [ titulo , url , 'thevideome' ] )
             encontrados.add(url)
         else:
-            logger.info("  url duplicada=" + url)
+            logger.info("  url duplicada="+url)
+            
+    patronvideos  = 'thevideo.me/([a-z0-9A-Z]+)'
+    logger.info("pelisalacarta.servers.thevideome find_videos #"+patronvideos+"#")
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+
+    for match in matches:
+        titulo = "[thevideo.me]"
+        url = "http://thevideo.me/embed-"+match+".html"
+        if url not in encontrados:
+            logger.info("  url="+url)
+            devuelve.append( [ titulo , url , 'thevideome' ] )
+            encontrados.add(url)
+        else:
+            logger.info("  url duplicada="+url)
     return devuelve
+
