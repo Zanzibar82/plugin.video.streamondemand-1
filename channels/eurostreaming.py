ad8e72536c0a4a5ba8f25b4f01eab3ff5f22c790
Dentaku65 <dentaku65@gmail.com>
2017-01-08 12:33:44 +0100

    fixed channel

--- channels/eurostreaming.py
+++ channels/eurostreaming.py
@@ -32,17 +32,11 @@ def isGeneric():
 def mainlist(item):
     logger.info("streamondemand.eurostreaming mainlist")
     itemlist = [Item(channel=__channel__,
-                     title="[COLOR azure]Film - Archivio[/COLOR]",
-                     action="peliculas",
-                     extra="movie",
-                     url="%s/category/film-in-streaming-vk-putlocker/" % host,
-                     thumbnail="http://repository-butchabay.googlecode.com/svn/branches/eden/skin.cirrus.extended.v2/extras/moviegenres/All%20Movies.png"),
-                Item(channel=__channel__,
                      title="[COLOR azure]Serie TV[/COLOR]",
                      action="serietv",
                      extra='serie',
                      url="%s/category/serie-tv-archive/" % host,
-                     thumbnail="http://xbmc-repo-ackbarr.googlecode.com/svn/trunk/dev/skin.cirrus%20extended%20v2/extras/moviegenres/New%20TV%20Shows.png"),
+                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
                 Item(channel=__channel__,
                      title="[COLOR azure]Anime / Cartoni[/COLOR]",
                      action="serietv",
@@ -56,64 +50,6 @@ def mainlist(item):
                      thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")]
 
     return itemlist
-
-
-def peliculas(item):
-    logger.info("streamondemand.eurostreaming peliculas")
-    itemlist = []
-
-    # Descarga la pagina
-    data = scrapertools.cache_page(item.url)
-
-    # Extrae las entradas (carpetas)
-    patron = '<div class="post-thumb">\s*'
-    patron += '<a href="?([^>"]+)"?.*?title="?([^>"]+)"?.*?<img.*?src="([^>"]+)'
-    matches = re.compile(patron, re.DOTALL).findall(data)
-
-    for scrapedurl, scrapedtitle, scrapedthumbnail in matches:
-        scrapedplot = ""
-        scrapedtitle = scrapedtitle.replace("Streaming", "")
-        scrapedtitle = scrapedtitle.replace("streaming", "")
-        scrapedtitle = scrapedtitle.replace("streaming ITA", "")
-        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle.replace(" ITA", ""))
-        if scrapedtitle.startswith("Link to "):
-            scrapedtitle = scrapedtitle[8:]
-        if (DEBUG): logger.info(
-            "title=[" + scrapedtitle + "], url=[" + scrapedurl + "], thumbnail=[" + scrapedthumbnail + "]")
-        itemlist.append(infoSod(
-            Item(channel=__channel__,
-                 action="findvideos",
-                 fulltitle=scrapedtitle,
-                 show=scrapedtitle,
-                 title=scrapedtitle,
-                 url=scrapedurl,
-                 thumbnail=scrapedthumbnail,
-                 plot=scrapedplot,
-                 extra=item.extra,
-                 folder=True), tipo='movie'))
-
-    # Extrae el paginador
-    patronvideos = '<a class="next page-numbers" href="?([^>"]+)">Avanti &raquo;</a>'
-    matches = re.compile(patronvideos, re.DOTALL).findall(data)
-
-    if len(matches) > 0:
-        scrapedurl = urlparse.urljoin(item.url, matches[0])
-        itemlist.append(
-            Item(channel=__channel__,
-                 action="HomePage",
-                 title="[COLOR yellow]Torna Home[/COLOR]",
-                 folder=True)),
-        itemlist.append(
-            Item(channel=__channel__,
-                 action="peliculas",
-                 title="[COLOR orange]Successivo >>[/COLOR]",
-                 url=scrapedurl,
-                 thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
-                 extra=item.extra,
-                 folder=True))
-
-    return itemlist
-
 
 def serietv(item):
     logger.info("streamondemand.eurostreaming peliculas")
