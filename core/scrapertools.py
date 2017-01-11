52474dec3730bc145c0bb32fbed88decbce6674e
Dentaku65 <dentaku65@gmail.com>
2017-01-11 20:36:39 +0100

    raise time

--- core/scrapertools.py
+++ core/scrapertools.py
@@ -55,7 +55,7 @@ COOKIES_PATH = os.path.join(config.get_data_path(), "cookies")
 if not os.path.exists(COOKIES_PATH):
   os.mkdir(COOKIES_PATH)
 
-DEFAULT_TIMEOUT = 45
+DEFAULT_TIMEOUT = 60
 
 DEBUG = False
 
