bf1816b56fd51463f38823d20271c3fb64383484
DrZ3r0 <DrZ3r0@suremail.info>
2017-02-22 19:57:06 +0100

    ripristino host, per anime va bene .cc

--- channels/cb01anime.py
+++ channels/cb01anime.py
@@ -18,7 +18,7 @@ __type__ = "generic"
 __title__ = "CineBlog01 Anime"
 __language__ = "IT"
 
-host = "http://www.cineblog01.uno"
+host = "http://www.cineblog01.cc"
 
 headers = [['Upgrade-Insecure-Requests', '1'],
            ['User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36'],
