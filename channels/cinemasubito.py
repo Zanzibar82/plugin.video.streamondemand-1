15e32394bb4ee4e926cb4584e737982523c44e21
Kevin <gtatruth11943@gmail.com>
2017-02-24 15:32:34 +0100

    Fix dominio

--- channels/cinemasubito.py
+++ channels/cinemasubito.py
@@ -22,7 +22,7 @@ __language__ = "IT"
 
 DEBUG = config.get_setting("debug")
 
-host = "http://www.cinemasubito.net"
+host = "https://www.cinemasubito.link"
 
 headers = [
     ['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0'],
