8b15acdd9f497bb68fc6623d32a89cf2e930896d
Dentaku65 <dentaku65@gmail.com>
2017-04-21 23:07:12 +0200

    fix

--- servers/youwatch.py
+++ servers/youwatch.py
@@ -1,48 +1,44 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------
 # streamondemand - XBMC Plugin
-# Server per youwatch
-# http://www.mimediacenter.info/foro/viewforum.php?f=36
+# Conector para youwatch
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 # ------------------------------------------------------------
 
 import re
-import urllib
 
-from lib import jsunpack
 from core import logger
 from core import scrapertools
+from core import httptools
 
 
 def test_video_exists(page_url):
-    logger.info("youwatch test_video_exists(page_url='%s')" % page_url)
+    logger.info("(page_url='%s')" % page_url)
+    data = httptools.downloadpage(page_url).data
+    if "File Not Found" in data:
+        return False, "[Youwatch] File cancellato"
+
+    url_redirect = scrapertools.find_single_match(data, '<iframe src="([^"]+)"')
+    data = httptools.downloadpage(url_redirect).data
+    if "We're sorry, this video is no longer available" in data:
+        return False, "[Youwatch]  File cancellato"
+
     return True, ""
 
 
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
-    logger.info("youwatch get_video_url(page_url='%s')" % page_url)
+    logger.info("(page_url='%s')" % page_url)
 
-    data = ''
-    patron_new_url = '<iframe\s+src\s*=\s*"([^"]+)'
+    data = httptools.downloadpage(page_url).data
+    url_redirect = scrapertools.find_single_match(data, '<iframe src="([^"]+)"')
+    data = httptools.downloadpage(url_redirect).data
 
-    while page_url != "":
-        headers = [
-            ['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:39.0) Gecko/20100101 Firefox/39.0'],
-            ['Accept-Encoding', 'gzip, deflate'],
-            ['Referer', page_url]
-        ]
-        data = scrapertools.cache_page(page_url, headers=headers)
-        page_url = scrapertools.find_single_match(data, patron_new_url)
-        page_url = re.sub("\n|\r|\t", "", page_url)
-
-    media_url = scrapertools.find_single_match(data, 'file\s*:\s*"([^"]+)')
-    if media_url == '':
-        data = scrapertools.find_single_match(data, '(eval\(function.*?)</script>')
-        data = jsunpack.unpack(data)
-        media_url = scrapertools.find_single_match(data, 'file\s*:\s*"([^"]+)')
-    video_urls = [[scrapertools.get_filename_from_url(media_url)[-4:] + " [youwatch]", media_url + '|' + urllib.urlencode(dict(headers))]]
+    url = scrapertools.get_match(data, '{file:"([^"]+)"')
+    video_url = "%s|Referer=%s" % (url, url_redirect)
+    video_urls = [[scrapertools.get_filename_from_url(url)[-4:] + " [youwatch]", video_url]]
 
     for video_url in video_urls:
-        logger.info("[youwatch.py] %s - %s" % (video_url[0], video_url[1]))
+        logger.info("%s - %s" % (video_url[0], video_url[1]))
 
     return video_urls
 
@@ -52,13 +48,15 @@ def find_videos(data):
     encontrados = set()
     devuelve = []
 
-    patronvideos = '(?://|\.)(?:youwatch\.org||chouhaa\.info)/(?:embed-)?([A-Za-z0-9]+)'
-    logger.info("youwatch find_videos #" + patronvideos + "#")
+    # http://chouhaa.info/embed-ihc21y2tqpnt.html
+    # http://youwatch.org/ihc21y2tqpnt
+    patronvideos = 'http://(?:youwatch.org|chouhaa.info)/(?:embed-|)([a-z0-9]+)'
+    logger.info("#" + patronvideos + "#")
     matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
-    for media_id in matches:
+    for match in matches:
         titulo = "[youwatch]"
-        url = 'http://youwatch.org/embed-%s.html' % media_id
+        url = "http://youwatch.org/embed-%s.html" % match
         if url not in encontrados:
             logger.info("  url=" + url)
             devuelve.append([titulo, url, 'youwatch'])
@@ -67,3 +65,4 @@ def find_videos(data):
             logger.info("  url duplicada=" + url)
 
     return devuelve
+
